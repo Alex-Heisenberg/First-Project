a08e4bde0318dd272893fa1861576cc26bad87dc
Alex <79204233333@ya.ru>
2024-08-08 12:25:16 +0200

    first commit

---
+++ myscript.js
@@ -0,0 +1,23 @@
+"use strict"
+const namberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: namberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцениtе его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцениtе его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
